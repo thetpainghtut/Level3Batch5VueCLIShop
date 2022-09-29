3889d9977985e6762fafd2c463ed6560f53a7dc1
Thet Paing Htut <thetpainghtut.bf@gmail.com>
2022-09-29 22:02:49 +0630

    added card component

--- src/views/TestingView.vue
+++ src/views/TestingView.vue
@@ -8,31 +8,7 @@
         class="row gx-4 gx-lg-5 row-cols-2 row-cols-md-3 row-cols-xl-4 justify-content-center"
       >
         <div class="col mb-5" v-for="(row,index) in products" :key="index">
-          <div class="card h-100">
-            <!-- Product image-->
-            <img
-              class="card-img-top"
-              :src="row.image"
-              alt="..."
-            />
-            <!-- Product details-->
-            <div class="card-body p-4">
-              <div class="text-center">
-                <!-- Product name-->
-                <h5 class="fw-bolder">{{row.title}}</h5>
-                <!-- Product price-->
-                ${{row.price}}
-              </div>
-            </div>
-            <!-- Product actions-->
-            <div class="card-footer p-4 pt-0 border-top-0 bg-transparent">
-              <div class="text-center">
-                <a class="btn btn-outline-dark mt-auto" href="#"
-                  >View options</a
-                >
-              </div>
-            </div>
-          </div>
+          <Card :product="row"/>
         </div>
       </div>
     </div>
@@ -41,6 +17,7 @@
 
 <script>
 import Header from "@/components/Header.vue";
+import Card from "@/components/Card.vue";
 const axios = require("axios");
 
 export default {
@@ -56,7 +33,7 @@ export default {
             this.products = response.data;
         });
     },
-    components: { Header }
+    components: { Header, Card }
 };
 </script>
 
